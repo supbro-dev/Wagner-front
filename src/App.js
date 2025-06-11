afe699c82fdea25841b40365fb82cbc190f23667
jinzhu <bigbullyrn@gmail.comm>
2025-06-11 15:03:11 +0800

    前后端调通

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import EfficiencySummary from './efficiency_summary/efficiency_summary'; // 导入组件
+import EfficiencySummary from './employee_efficiency/employee_efficiency'; // 导入组件
 import './App.css';
 
 function App() {
