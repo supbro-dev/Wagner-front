2426210e4ca1a74854dbc1d92025bc848a625635
jinzhu <bigbullyrn@gmail.comm>
2025-06-20 09:58:10 +0800

    员工监控完成

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import EfficiencySummary from './employee_efficiency/employee_efficiency'; // å¯
 import TimeOnTask from './time_on_task/time_on_task';
 import Test from './test/test';
 import WorkplaceEfficiency from './workplace_efficiency/workplace_efficiency';
+import EmployeeStatus from './employee_status/employee_status';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <EfficiencySummary />
+            {/*<EfficiencySummary />*/}
             {/*<WorkplaceEfficiency />*/}
             {/*<TimeOnTask />*/}
+            <EmployeeStatus />
             {/*<Test />*/}
         </div>
     );
